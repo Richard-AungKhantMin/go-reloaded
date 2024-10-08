6473b0cebecef0ba84d27aede6189dff02a1d4f3
amin <aungkhantmin.dev@gmail.com>
2024-10-08 17:20:50 +0300

    a

--- MyLab.go
+++ MyLab.go
@@ -13,29 +13,45 @@ func MyLab(sentence string) string {
 	}
 
 	var QuoteCount int
-	for _, eachWord := range words {
-		switch eachWord {
-		case "a", "A":
+	var num
 
-		case "(hex)":s
+	for i, eachWord := range words {
+		switch eachWord {
+
+		case "a", "A":
+			if strings.Contains("aeiouhAEIOUH", string(words[i][0])){
+				eachWord = eachWord + "n"
+			}
+
+		case "(hex)":
+			words[i-1] = HexToDec(words[i-1])
 
 		case "(bin)":
+			words[i-1] = BinaryToDec(words[i-1])
 
 		case "(up)":
+			words[i-1] = Upper(words[i-1])
 
 		case "(low)":
+			words[i-1] = Lower(words[i-1])
 
 		case "(cap)":
+			words[i-1] = CapCap(words[i-1])
 
 		case "(low,":
+
 
 		case "(up":
 
 		case "(cap":
 
-		case ""
+		case "'":
+
+		default:
+
 		}
 	}
+
 
 }
 
