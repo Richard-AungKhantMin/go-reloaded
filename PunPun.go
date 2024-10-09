4a0cf56d5f1d360a822c8d2c6259ea6d9172873f
Richard-AungKhantMin <aungkhantmin.dev@gmail.com>
2024-10-09 22:12:24 +0300

    a

--- PunPun.go
+++ PunPun.go
@@ -4,9 +4,9 @@ import "strings"
 
 func PunPun(words []string) []string {
 
-	for temp := 0; temp < len(words); temp++ {
-		for i := 0; i < len(words); i++ {
-			for i > 0 && len(words[i]) > 0 && strings.Contains(".,!?;:", string(words[i][0])) {
+	for temp := 1; temp < len(words); temp++ {
+		for i := 1; i < len(words); i++ {
+			for len(words[i]) > 0 && strings.Contains(".,!?;:", string(words[i][0])) {
 				words[i-1] = words[i-1] + string(words[i][0])
 				words[i] = words[i][1:]
 			}
